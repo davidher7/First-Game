ba8804f9950557a18dd0ca30c6e75d49234e26d6
David <davidher.713@gmail.com>
2019-03-19 12:11:32 -0700

    Test
    Tested Various Settings

---
+++ First-Game/Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float playerSpeed;
+
+	// Update is called once per frame
+	void Update () 
+    {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+
+        transform.Translate(x, y, 0);
+		
+	}
+}
