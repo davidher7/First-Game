b51724b03491ffec6a430a4caff50c54c4fbf7f9
David <davidher.713@gmail.com>
2019-03-21 10:39:56 -0700

    Added the basics
    Began creating a Start Screen
    Player can now move

---
+++ First-Game/Assets/Scripts/Move.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+    public Rigidbody2D rb;
+    public float playerSpeed;
+    public float jumpForce;
+
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
+    bool isGrounded = true;
+
+    // Update is called once per frame
+    void FixedUpdate () 
+    {
+        // Movement
+        float moveHorizontal = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+
+        transform.Translate(moveHorizontal, 0, 0);
+
+        // Jump
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.velocity += Vector2.up * jumpForce;
+            isGrounded = false;
+
+        }
+
+        // Better Jump
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButtonDown("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+
+        }
+    }
+
+    // Jump Limiter
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded |= collision.gameObject.tag == "Ground";
+
+    }
+}
