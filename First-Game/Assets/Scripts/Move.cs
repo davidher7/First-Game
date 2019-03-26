9eca71f4ff76fea3e65ca4cea07cf4a171950a7f
davidher7 <davidher7@users.noreply.github.com>
2019-03-26 10:10:36 -0700

    Added groundCheck()
    Uses RaycastAll to check the ground

--- First-Game/Assets/Scripts/Move.cs
+++ First-Game/Assets/Scripts/Move.cs
@@ -10,7 +10,18 @@ public class Move : MonoBehaviour
 
     public float fallMultiplier;
     public float lowJumpMultiplier;
-    bool isGrounded = true;
+    [SerializeField] bool isGrounded = true;
+    private float jumpTimer = -1;
+
+    private void Update()
+    {
+        if(jumpTimer > 0)
+        {
+            jumpTimer -= Time.deltaTime;
+
+        }
+
+    }
 
     // Update is called once per frame
     void FixedUpdate () 
@@ -21,9 +32,10 @@ public class Move : MonoBehaviour
         transform.Translate(moveHorizontal, 0, 0);
 
         // Jump
-        if (Input.GetButton("Jump") && isGrounded)
+        if (Input.GetButton("Jump") && isGrounded &&  jumpTimer < 0)
         {
             isGrounded = false;
+            jumpTimer = .25f;
             rb.velocity += Vector2.up * jumpForce;
 
         }
@@ -39,6 +51,41 @@ public class Move : MonoBehaviour
             rb.velocity += Vector2.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
 
         }
+
+        groundCheck();
+        
+    }
+
+    void groundCheck()
+    {
+        if(isGrounded == false)
+        {
+            return;
+
+        }
+
+        var raycastHits = Physics2D.RaycastAll(transform.position, Vector2.down, .75f);
+        bool foundGround = false;
+
+        for (var count = 0; count < raycastHits.Length; count++)
+        {
+
+            if (raycastHits[count].transform.tag == "Ground")
+            {
+                foundGround = true;
+
+                break;
+
+            }
+
+        }
+
+        if(foundGround == false)
+        {
+            isGrounded = false;
+
+        }
+
     }
 
     // Jump Limiter
